26ef96f6013b3ca3bfa183697b6d7efa12cfa361
code-lucky <2924249993@qq.com>
2023-08-26 00:15:06 +0800

    页面新增

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,10 +1,59 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 
+const formState = ref({
+    username:'',
+    password:'',
+
+})
 </script>
 
 <template>
     <div>
-        <a-button type="primary" block>Primary</a-button>
+        <h3>会议室预定系统</h3>
+        <a-form
+            :model="formState"
+            name="basic"
+            :label-col="{ span: 8 }"
+            :wrapper-col="{ span: 16 }"
+            autocomplete="off"
+        >
+            <a-form-item
+            label="邮箱"
+            name="username"
+            :rules="[{ required: true, message: 'Please input your username!' }]"
+            >
+                <a-input v-model:value="formState.username" />
+            </a-form-item>
+
+            <a-form-item
+            label="验证码"
+            name="username"
+            :rules="[{ required: true, message: 'Please input your username!' }]"
+            >
+                <a-input v-model:value="formState.username" />
+            </a-form-item>
+
+            <a-form-item
+            label="密码"
+            name="password"
+            :rules="[{ required: true, message: 'Please input your password!' }]"
+            >
+                <a-input-password v-model:value="formState.password" />
+            </a-form-item>
+
+            <a-form-item
+            label="确认密码"
+            name="password"
+            :rules="[{ required: true, message: 'Please input your password!' }]"
+            >
+                <a-input-password v-model:value="formState.password" />
+            </a-form-item>
+
+            <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+                <a-button type="primary" html-type="submit">修改</a-button>
+            </a-form-item>
+        </a-form>
     </div>
 </template>
 
