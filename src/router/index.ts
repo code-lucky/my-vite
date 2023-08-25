b4cc45465af3084aaeb750ab32903c95368aa9d7
code-lucky <2924249993@qq.com>
2023-08-26 00:04:31 +0800

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,27 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+const routes: Array<RouteRecordRaw>  = [
+    {
+        path:'/',
+        name:'index',
+        component:() => import('@/views/index/index.vue'),
+        hidden: true
+    }
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+        // 始终滚动到顶部
+        return { top: 0 }
+    },
+});
+
+router.beforeEach((_to, _from) => {
+    // 路由守卫逻辑
+    // 通过 return turn;
+    // 不通过 return false;
+    return true;
+});
+
+export default router;
---
+++ src/views/index/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <a-button type="primary" block>Primary</a-button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
